731eb34a88593952bf46482e5cf0710fefea7131
benhassenwael <greenw2012@gmail.com>
2022-02-02 15:31:05 +0100

    Initial commit

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" 0x19. Making Change
+"""
+
+
+def makeChange(coins, total):
+    """ Given a pile of coins of different values,
+        determine the fewest number of coins needed
+        to meet a given amount total
+    """
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for a in range(1, total + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - c])
+
+    return dp[total] if dp[total] != total + 1 else -1
